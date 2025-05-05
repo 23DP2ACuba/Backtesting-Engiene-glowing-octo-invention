af852d1aaa6fbfd92960fd52f3f0c44440617710
unknown <A230926AC@rvt.lv>
2025-05-05 23:37:30 +0300

    SMA

--- indicators/indicators.rs
+++ indicators/indicators.rs
@@ -1,3 +1,23 @@
-pub fn indicators() {
-    println!("indicators: SMA, EMA, RSI, MACD...");
+use crate::data_reader::data::data::DataFeed;
+pub struct Indicators {
+    pub ohlcv: DataFeed,
+} 
+impl Indicators {
+    pub fn SMA(&mut self, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let mut sma = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let data = &self.ohlcv.get_ohlcv().Close;
+        let mut sum = 0.0;
+        for i in 0..period{
+            sum += data[i as usize];
+        }
+        sma[(period - 1) as usize] = sum / period as f64;
+
+        for i in period..data.len() as i64 {
+            sum -= data[(i-period) as usize];
+            sum += data[i as usize];
+            sma[i as usize] = sum /period as f64;
+        }
+
+        Ok(sma)
+    }
 }
--- main.rs
+++ main.rs
@@ -3,13 +3,18 @@ mod engn_code;
 mod indicators;
 use csv::Reader;
 use data_reader::data::data::DataFeed;
+use indicators::indicators::Indicators;
 use engn_code::engiene::backtest::Backtest;
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut df = DataFeed::new();
-    
     df.read_csv("BTC-USD.csv")?;
+    let mut i = Indicators{
+        data: df.get_ohlcv(),
+    };
+    
+    
 
     let size = df.get_size()?;
     println!("{}", size);
@@ -18,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     
     let ohlcv = df.get_ohlcv();
     println!("{:?}", ohlcv.Date[0]);
-
+    _ = i.SMA(10);
     Backtest::new(ohlcv);
     Ok(())
 }
