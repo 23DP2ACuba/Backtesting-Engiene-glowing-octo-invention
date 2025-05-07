d1e7cdd2498106bd8302b4d50971811b4f2ba9eb
unknown <A230926AC@rvt.lv>
2025-05-07 14:46:58 +0300

    finnished indicators

--- indicators/indicators.rs
+++ indicators/indicators.rs
@@ -20,4 +20,160 @@ impl Indicators {
 
         Ok(sma)
     }
+
+    pub fn EMA(&mut self, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let mut ema = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let data = &self.ohlcv.get_ohlcv().Close;
+
+        let mut sum = 0.0;
+
+        for i  in 0..period {
+            sum += data[i as usize];
+        }
+
+        ema[(period - 1) as usize] = sum / period as f64;
+
+        for i in period..data.len() as i64{
+            let i = i as usize;
+            ema[i] = (data[i] * (2 / (period + 1)) as f64) + (ema[i - 1] * (1 - (2 / (period + 1))) as f64);
+        }
+        Ok(ema)
+    
+    }
+
+
+    fn _MACD(&mut self, fast: i64, slow: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let ohlcv = self.ohlcv.get_ohlcv();
+        let close = &ohlcv.Close;
+        let len = close.len();
+    
+        let ema_fast = self.EMA(fast)?;
+        let ema_slow = self.EMA(slow)?;
+    
+        if ema_fast.len() != len || ema_slow.len() != len {
+            return Err("EMA lengths do not match OHLCV length".into());
+        }
+    
+        let macd: Vec<f64> = ema_fast.iter()
+            .zip(ema_slow.iter())
+            .map(|(short, long)| short - long)
+            .collect();
+    
+        Ok(macd)
+    }
+
+    fn _SIGNAL(&mut self, period: i64, fast: i64, slow: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let macd_line = self._MACD(fast, slow)?; 
+        let signal_line = Self::ema_from_vec(&macd_line, period)?;
+        Ok(signal_line)
+    }
+
+    fn ema_from_vec(data: &Vec<f64>, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        if data.len() < period as usize {
+            return Err("Not enough data to compute EMA".into());
+        }
+    
+        let mut ema = Vec::with_capacity(data.len());
+        let k = 2.0 / (period as f64 + 1.0);
+    
+        let mut prev_ema = data[0];
+        ema.push(prev_ema);
+    
+        for i in 1..data.len() {
+            let current = data[i];
+            prev_ema = current * k + prev_ema * (1.0 - k);
+            ema.push(prev_ema);
+        }
+    
+        Ok(ema)
+    }
+
+    pub fn MACD(&mut self, period: i64, fast: i64, slow: i64)  -> Result<(Vec<f64>, Vec<f64>), Box<dyn std::error::Error>> {
+        let macd = self._MACD(fast, slow)?;
+        let signal = self._SIGNAL(period, fast, slow)?;
+        Ok((macd, signal))
+    }
+
+    pub fn ATD(&mut self, period:i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let mut atr = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let data = self.ohlcv.get_ohlcv().Close;
+        let mut tr = 0f64;
+        for i in period..data.len() as i64{
+            tr = f64::max(data[i as usize] - data[(i - 1) as usize], f64::max(data[i as usize] - self.ohlcv.get_ohlcv().High[(i - 1) as usize], self.ohlcv.get_ohlcv().Low[(i - 1) as usize] - data[i as usize]));
+            atr[i as usize ] = (atr[(i - 1) as usize] * (period - 1) as f64 + tr) / period as f64;
+        }
+
+        Ok(atr)
+    }
+
+    pub fn BBANDS(&mut self, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let data = self.ohlcv.get_ohlcv().Close;
+        let sma = self.SMA(period)?;
+        let mut bbands = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let mut stddev = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+
+        for i in period..data.len() as i64 {
+            let mut sum = 0.0;
+            for j in (i - period)..i {
+                let diff = data[j as usize] - sma[i as usize];
+                sum += diff * diff;
+            }
+            stddev[i as usize] = (sum / period as f64).sqrt();
+        }
+    
+        for i in 0..data.len() {
+            if stddev[i] != 0.0 {
+                bbands[i] = (data[i] - sma[i]) / (2.0 * stddev[i]);
+            } else {
+                bbands[i] = 0.0; 
+            }
+        }
+    
+        Ok(bbands)
+    }
+
+    pub fn STOCHASTIC(&mut self, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let data = self.ohlcv.get_ohlcv().Close;
+        let mut stochastic = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let mut min = 0f64;
+        let mut max = 0f64;
+        
+        for i in period..data.len() as i64 {
+            min = data[(i-period) as usize];
+            max = data[(i-period) as usize];
+
+            for j in (i-period)..i {
+                if data[j as usize] < min {
+                    min = data[j as usize];
+                } else if data[j as usize] > max {
+                    max = data[j as usize];
+                }
+            }
+            stochastic[i as usize] = (data[i as usize] - min) / (max - min);
+        }
+        Ok(stochastic)
+    }
+
+    pub fn WILLIAMS(&mut self, period: i64) -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+        let data = self.ohlcv.get_ohlcv().Close;
+        let mut williams = vec![0.0; self.ohlcv.get_ohlcv().Date.len()];
+        let mut min = 0f64;
+        let mut max = 0f64;
+        
+        for i in period..data.len() as i64 {
+            min = data[(i-period) as usize];
+            max = data[(i-period) as usize];
+
+            for j in (i-period)..i {
+                if data[j as usize] < min {
+                    min = data[j as usize];
+                } else if data[j as usize] > max {
+                    max = data[j as usize];
+                }
+            }
+            williams[i as usize] = (max - data[i as usize]) / (max - min);
+        }
+        Ok(williams)
+    }
+
 }
