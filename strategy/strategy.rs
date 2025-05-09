519daae7e1731d0a83387d694d337f79a9213fa7
unknown <A230926AC@rvt.lv>
2025-05-09 22:44:13 +0300

    strategy

---
+++ strategy/strategy.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+use indicators::Indicator;
+use crate::data::Data;
+use engnine::Engine;
+
+pub mod strategy {
+    struct Prams {
+        data: HashMap<String, _>,
+
+    }
+    impl Prams {
+        
+    }
+}
